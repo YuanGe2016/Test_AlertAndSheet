3d1567ffca306661c2a615fbaab5cd7d75b08688
lujiayuan <lujiayuan@bytedance.com>
2020-05-21 16:38:16 +0800

    Initial Commit

---
+++ AlertAndSheet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AlertAndSheet
+//
+//  Created by Bytedance on 2020/5/21.
+//  Copyright © 2020 Bytedance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
