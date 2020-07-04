633c542ff1945f6cbccf23686abae94ddc874644
陆家源 <lujiayuan@bytedance.com>
2020-07-04 18:56:19 +0800

    add singleton test

---
+++ AlertAndSheet/KVCTest.m
@@ -0,0 +1,13 @@
+//
+//  KVCTest.m
+//  AlertAndSheet
+//
+//  Created by Bytedance on 2020/7/4.
+//  Copyright © 2020 Bytedance. All rights reserved.
+//
+
+#import "KVCTest.h"
+
+@implementation KVCTest
+
+@end
